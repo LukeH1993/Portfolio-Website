da49a88b2c4cb363479953a857d9c66c51f9bee5
Luke Heudebourck <lwheudebourck@gmail.com>
2020-01-05 16:45:28 +0000

    Updates to Website

--- main.js
+++ main.js
@@ -1,33 +1,7 @@
-var slidesIndex = 1;
-showSlides(slidesIndex);
-
-// Next and previous controls.
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls.
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-// Slideshow controls.
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("slides");
-    var dots = document.getElementsByClassName("dots");
-    if (n > slides.length) {
-        slideIndex = 1;
-    }
-    if (n < 1) {
-        slideIndex = slides.length;
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-}
+function myFunction(imgs) {
+    var expandImg = document.getElementById("expandedImg");
+    var imgText = document.getElementById("imgtext");
+    expandImg.src = imgs.src;
+    imgText.innerHTML = imgs.alt;
+    expandImg.parentElement.style.display = "block";
+  }
