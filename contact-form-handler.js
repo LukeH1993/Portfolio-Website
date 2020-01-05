571b514f568b7550bd3f8071203fdfd3120c0632
Luke Heudebourck <lwheudebourck@gmail.com>
2020-01-05 16:51:24 +0000

    Removed unsued file.

--- contact-form-handler.js
+++
@@ -1,4 +0,0 @@
-var frmvalidator = new Validator("contact form");
-frmvalidator.addValidation("name", "req", "Please provide your name");
-frmvalidator.addValidation("email", "req", "Please provide your email address");
-frmvalidator.addValidation("email", "email", "Please enter a valid email address");
